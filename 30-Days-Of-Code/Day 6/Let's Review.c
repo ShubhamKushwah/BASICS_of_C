1960197c5716781dfa152418df1bc3cdf962410e
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-02-05 16:46:35 +0530

    Update Let's Review.c

--- 30-Days-Of-Code/Day 6/Let's Review.c
+++ 30-Days-Of-Code/Day 6/Let's Review.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-#include <math.h>   //Not Required
-#include <stdlib.h> //Not Required
+#include <math.h>   /*Not Required*/
+#include <stdlib.h> /*Not Required*/
 
 int main(){
 
     int test, i;
-    char str[10000];    //since the maximum size given there is 10000 characters length
+    char str[10000];    /*since the maximum size given there is 10000 characters length*/
     scanf("%d", &test);
     for(;test>0;test--){
         scanf("%s",str);
-        //strlen(<string name>); is used to find the length of the string.
+        /*strlen(<string name>); is used to find the length of the string.*/
         for(i=0;i<strlen(str);i++)
         {   if(i%2==0)
             printf("%c", str[i]);
