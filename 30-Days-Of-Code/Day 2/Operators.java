900e1696e4d83ee211a0bf7aebb6d2e28f9ea324
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-02-05 16:42:16 +0530

    Update Operators.java

--- 30-Days-Of-Code/Day 2/Operators.java
+++ 30-Days-Of-Code/Day 2/Operators.java
@@ -3,14 +3,14 @@ public class Arithmetic{
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double mealCost = scan.nextDouble(); // original meal price
-        int tipPercent = scan.nextInt(); // tip percentage
-        int taxPercent = scan.nextInt(); // tax percentage
-        scan.close(); //close the scanner variable
+        double mealCost = scan.nextDouble(); /*original meal price*/
+        int tipPercent = scan.nextInt(); /*tip percentage*/
+        int taxPercent = scan.nextInt(); /*tax percentage*/
+        scan.close(); /*close the scanner variable*/
       
-        double total = mealCost + tipPercent*mealCost*0.01 + taxPercent*mealCost*0.01;//calculation code.
+        double total = mealCost + tipPercent*mealCost*0.01 + taxPercent*mealCost*0.01;/*calculation code.*/
       
-        // cast the result of the rounding operation to an int and save it as totalCost 
+        /* cast the result of the rounding operation to an int and save it as totalCost */
         int totalCost = (int) Math.round(total);
       
         System.out.println("The total meal cost is " + totalCost + " dollars.");
