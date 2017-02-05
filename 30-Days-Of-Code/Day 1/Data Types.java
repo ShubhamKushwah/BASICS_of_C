6612d51b7bc2d5a1e9a9ca8092e3ced2fb1e3ab0
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-02-05 16:40:42 +0530

    Update Data Types.java

--- 30-Days-Of-Code/Day 1/Data Types.java
+++ 30-Days-Of-Code/Day 1/Data Types.java
@@ -1,8 +1,8 @@
-import java.io.*;   //Not required
+import java.io.*;   /*Not required*/
 import java.util.*; 
-import java.text.*;   //Not required
-import java.math.*;   //Not required
-import java.util.regex.*;   //Not required
+import java.text.*;   /*Not required*/
+import java.math.*;   /*Not required*/
+import java.util.regex.*;   /*Not required*/
 
 public class Solution {
 	
@@ -17,14 +17,14 @@ public class Solution {
         double b;
         String c;
         
-        a = scan.nextInt();   //for integer input
-        b = scan.nextDouble();  //for double input
-        scan.nextLine();    //we need to do this whenever we take input of a string just after some another input
-        c = scan.nextLine();    //to take string input
+        a = scan.nextInt();   /*for integer input*/
+        b = scan.nextDouble();  /*for double input*/
+        scan.nextLine();    /*we need to do this whenever we take input of a string just after some another input*/
+        c = scan.nextLine();    /*to take string input*/
 
-        System.out.println(a+i);    //to print the sum
-        System.out.println(b+d);    //to print the sum
-        System.out.println(s+c);    //to print the combined strings
+        System.out.println(a+i);    /*to print the sum*/
+        System.out.println(b+d);    /*to print the sum*/
+        System.out.println(s+c);    /*to print the combined strings*/
  
         scan.close();
     }
