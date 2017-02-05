ecbd69f168c61049e4ec7462d54659b4c16df825
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-02-05 12:09:51 +0530

    Create Operators.cpp

---
+++ 30-Days-Of-Code/Day 2/Operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h> //since we have used the round function.
+
+using namespace std;
+
+int main()
+{
+    double cost, total=0;
+    int tip, tax;
+
+    cin>>cost>>tip>>tax;
+
+    total = cost + tip*cost/100 + tax*cost/100; //calculation
+   
+    cout<<"The total meal cost is "<<round(total)<<" dollars."; //to round off the answer 
+
+return 0;
+}
