b939cf1e38f7c58eb6d29cd747a11536604d14c1
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-02-05 16:45:39 +0530

    Update Loops.java

--- 30-Days-Of-Code/Day 5/Loops.java
+++ 30-Days-Of-Code/Day 5/Loops.java
@@ -1,14 +1,14 @@
 import java.io.*;
-import java.util.*;     //Not Required
-import java.text.*;     //Not Required
-import java.math.*;     //Not Required
-import java.util.regex.*;   //Not Required
+import java.util.*;     /*Not Required*/
+import java.text.*;     /*Not Required*/
+import java.math.*;     /*Not Required*/
+import java.util.regex.*;   /*Not Required*/
 
 public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();   //taking input in 'n'
+        int n = in.nextInt();   /*taking input in 'n'*/
         int i;
     
         //here the loop is.
