43d506fe15aa9820736d7e834a7ce8fe9b0cdca8
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-02-05 16:39:08 +0530

    Update Data Types.c

--- 30-Days-Of-Code/Day 1/Data Types.c
+++ 30-Days-Of-Code/Day 1/Data Types.c
@@ -12,9 +12,9 @@ int main() {
     double b;
     char c[100] = " ";
     scanf("%d%lf", &a, &b);
-    scanf("\n");    //to avoid the next line of above scanf to be inputted in  fgets as user input
+    scanf("\n");    /*to avoid the next line of above scanf to be inputted in  fgets as user input*/
     fgets(c, 100, stdin);
-    printf("%d\n%0.1lf\n%s%s", a+i, b+d, s, c); //0.1lf is to set the precision of decimal places to 1 only
+    printf("%d\n%0.1lf\n%s%s", a+i, b+d, s, c); /*0.1lf is to set the precision of decimal places to 1 only*/
     
         return 0;
 }
