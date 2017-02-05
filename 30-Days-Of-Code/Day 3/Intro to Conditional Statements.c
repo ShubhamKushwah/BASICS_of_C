7f5c80a18814ece7a2836002935e3e6d8da28430
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-02-05 12:21:51 +0530

    Create Intro to Conditional Statements.c

---
+++ 30-Days-Of-Code/Day 3/Intro to Conditional Statements.c
@@ -0,0 +1,24 @@
+#include <math.h>       //Not Required
+#include <stdio.h>      
+#include <string.h>     //Not Required
+#include <stdlib.h>     //Not Required
+#include <assert.h>     //Not Required
+#include <limits.h>     //Not Required
+#include <stdbool.h>    //Not Required
+
+int main(){
+    int N;
+    scanf("%d", &N);
+
+    if(N%2 == 0)
+    {   if(N>=2&&N<=5)
+            printf("Not Weird");
+        else if(N>=6&&N<=20)
+            printf("Weird");
+        else if(N>20)
+            printf("Not Weird");
+    }
+    else
+        printf("Weird");
+    return 0;
+}
