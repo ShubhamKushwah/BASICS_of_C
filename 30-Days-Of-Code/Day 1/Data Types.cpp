7d0e6e2faa2d7c9074baed7cbd48c79eb29d52b6
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-02-05 16:39:49 +0530

    Update Data Types.cpp

--- 30-Days-Of-Code/Day 1/Data Types.cpp
+++ 30-Days-Of-Code/Day 1/Data Types.cpp
@@ -1,6 +1,6 @@
 #include <iostream> 
-#include <iomanip>  //For setprecision()
-#include <limits>   //not required
+#include <iomanip>  /*For setprecision()*/
+#include <limits>   /*Not required*/
 
 using namespace std;
 
@@ -16,10 +16,10 @@ int main() {
 
     cin>>a>>b;
   
-    cin.ignore(256, '\n');  //this is used because whenever you use "getline" just after "cin" the user hits enter to commit changes to "cin" and the string catches it up, so to ignore that
+    cin.ignore(256, '\n');  /*this is used because whenever you use "getline" just after "cin" the user hits enter to commit changes to "cin" and the string catches it up, so to ignore that*/
     getline(cin, c);
     
-    cout<<a+i<<endl<<fixed<<setprecision(1)<<b+d<<endl<<s<<c;   //to print everything out, note that I have not copied that string c after that s, but just printed it after that
+    cout<<a+i<<endl<<fixed<<setprecision(1)<<b+d<<endl<<s<<c;   /*to print everything out, note that I have not copied that string c after that s, but just printed it after that*/
     
      return 0;
 }
