ee7957dd43c6aa8446096d19853c6fc13dd15cb8
SHAKTI SINGH <mohittanwar2323@gmail.com>
2017-10-20 16:09:56 +0530

    Recursion program day-9

---
+++ 30-Days-Of-Code/Day-9/recursion.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int factorial(int);
+
+int main() {
+    int number;
+    cin>>number;
+    cout<<factorial(number);
+            return 0;
+}
+int factorial(int number){
+    int temp;
+    if(number<=1) return 1; 
+    temp=number*factorial(number-1);
+    return temp;
+}
+
